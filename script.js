ccff7d0cadfb4f6b6f4a30802a876d2f47635a2c
yashh6707 <164030841+yashh6707@users.noreply.github.com>
2024-03-20 22:04:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,18 +18,18 @@ const febHolidays = [
     "And trust me I dont want anyone else",
     "There's no one better than you❤️",
     "You're the best baby!!",
-    "Or should I say, mo gro coco d'amour que j'adore a la folie XD",
-    "Wow!! Time flies, we've already been together for a month",
+    "Or should I say, You are my lucky charm",
+    "We've been connected since we met online",
     "There's some hardships,",
     "but we overcame most of them",
     "and it made me realised how important you are in my life❤️",
-    "This for you,",
-    "To l'amour",
-    "ek mo l'amour",
-    "fr 2 liv pomme d'amour🤣❤️",
-    "But anyways, Happy 1 month my sweet little girlfriend(wife🤭)",
+    "This one is only for you,",
+    "Baby",
+    "Tujhe jhumke pasand hai",
+    "Aur mujhe jhumko me tum💕✨",
+    "But anyways,You are my sweet little girlfriend(wife🤭)",
     "You're the love of my life,",
-    "I Love You So much❤️"
+    "I Love You So much💕"
   ];
   const ulEl = document.querySelector("ul");
   const d = new Date();
